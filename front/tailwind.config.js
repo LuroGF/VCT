808fd7ce0410e25391063ff03ad11d7af608d55b
alvaro.grueso <alvaro.grueso@mercanza.es>
2025-01-03 12:22:04 +0100

    Primera subida

---
+++ front/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Ajusta esta línea para que escanee tus archivos Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
